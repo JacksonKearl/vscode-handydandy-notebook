fd60e03abfbf8ef8a77fa91e2aae5aa162f2a65d
Jackson Kearl <jakearl@microsoft.com>
2020-11-10 15:44:38 -0800

    Add command for new blank notebook

--- src/extension.ts
+++ src/extension.ts
@@ -11,12 +11,13 @@ import { HandyDandyKernel } from './OmniExecutor';
 export function activate(context: vscode.ExtensionContext) {
 
 	context.subscriptions.push(
+		vscode.commands.registerCommand('handydandy-notebook.newNotebook', () =>
+			vscode.commands.executeCommand('workbench.action.files.newUntitledFile', { "viewType": "handydandy-notebook" })),
 		vscode.notebook.registerNotebookContentProvider(
 			'handydandy-notebook', new MarkdownProvider(), providerOptions),
 		vscode.notebook.registerNotebookKernelProvider(
 			{ viewType: 'handydandy-notebook' }, { provideKernels: () => [HandyDandyKernel] }));
 }
-
 
 // this method is called when your extension is deactivated
 export function deactivate() { }
